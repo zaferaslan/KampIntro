b7a712eb3b41e309094e0075c34d7a88a5fac87a
Zafer Aslan <zafer.aslan@ogr.dpu.edu.tr>
2021-02-14 11:37:22 +0300

    20 Ocak 2021

---
+++ GeneriscIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeneriscIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Zafer");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{   //İş yapan classım
+    class PrdocutManager //Ürünle ilgili operasyonlar var eğer service ve manager görürsen böyle bil
+    {   
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +" eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1+sayi2);
+        }
+    }
+}
